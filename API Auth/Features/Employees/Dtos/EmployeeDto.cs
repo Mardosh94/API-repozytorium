71049f88e2b0de25cb9f506a98e5474cfc6edc38
Mardosh94 <113902538+Mardosh94@users.noreply.github.com>
2024-11-26 11:54:35 +0100

    Commit próbny na gałęzi Marcin

---
+++ API Auth/Features/Employees/Dtos/EmployeeDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using API_Auth.Entities;
+
+namespace API_Auth.Features.Employees.Dtos
+{
+    public class EmployeeDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public AddressDto Address { get; set; }
+        public List<TimesheetDto>? Timesheets { get; set; }
+    }
+}
